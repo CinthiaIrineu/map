887fb61218082c16ef1b53f9b1024b18404a09d9
CinthiaIrineu <cinthia.irineu@hotmail.com>
2017-05-22 17:02:39 -0300

    Exercício XII - Observer
    Exercício da disciplina de MAP

---
+++ Exercicio XII - Observer/src/OctObserver.java
@@ -0,0 +1,11 @@
+public class OctObserver extends Observer{
+	
+    public OctObserver(Subject subject){
+        this.subject = subject;
+        this.subject.add(this);
+    }
+    
+    public void update() {
+        System.out.print(Integer.toHexString(subject.getState()));
+    }
+}
